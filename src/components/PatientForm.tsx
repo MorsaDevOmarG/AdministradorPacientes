486baf3912beb9a0885630235e7951633b2198bf
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-12-16 22:43:12 -0600

    Procesar el FORM con REACT HOOK FORM

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,7 +1,11 @@
 import { useForm } from "react-hook-form";
 
 export default function PatientForm() {
-  const { register } = useForm();
+  const { register, handleSubmit, formState: {errors} } = useForm();
+
+  const registerPatient = () => {
+    console.log('Nuevo paciente');
+  };
   
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5">
@@ -15,6 +19,7 @@ export default function PatientForm() {
       <form
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
         noValidate
+        onSubmit={handleSubmit(registerPatient)}
       >
         <div className="mb-5">
           <label htmlFor="name" className="text-sm uppercase font-bold">
@@ -29,6 +34,10 @@ export default function PatientForm() {
               required: "El nombre del paciente es obligatorio",
             })}
           />
+
+          {
+            errors.name?.message
+          }
         </div>
 
         <div className="mb-5">
