633969ed598d040bc53c2264d595ddd127c53bd9
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-12-16 23:05:27 -0600

    Agregando VALIDACION al FORM

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -33,20 +33,14 @@ export default function PatientForm() {
             placeholder="Nombre del Paciente"
             {...register("name", {
               required: "El nombre del paciente es obligatorio",
-              minLength: {value: 3, message: "El nombre es muy corto"}
+              minLength: { value: 3, message: "El nombre es muy corto" },
             })}
           />
 
           {/* {
             errors.name?.message
           } */}
-          {
-            errors.name && (
-              <Error>
-                {errors.name?.message?.toString()}
-              </Error>
-            )
-          }
+          {errors.name && <Error>{errors.name?.message?.toString()}</Error>}
         </div>
 
         <div className="mb-5">
@@ -58,7 +52,15 @@ export default function PatientForm() {
             className="w-full p-3  border border-gray-100"
             type="text"
             placeholder="Nombre del Propietario"
+            {...register("caretaker", {
+              required: "El nombre del propietario es obligatorio",
+              minLength: { value: 3, message: "El nombre es muy corto" },
+            })}
           />
+
+          {errors.caretaker && (
+            <Error>{errors.caretaker?.message?.toString()}</Error>
+          )}
         </div>
 
         <div className="mb-5">
@@ -70,7 +72,16 @@ export default function PatientForm() {
             className="w-full p-3  border border-gray-100"
             type="email"
             placeholder="Email de Registro"
+            {...register("email", {
+              required: "El Email es Obligatorio",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Email No Válido",
+              },
+            })}
           />
+
+          {errors.email && <Error>{errors.email?.message?.toString()}</Error>}
         </div>
 
         <div className="mb-5">
@@ -81,7 +92,12 @@ export default function PatientForm() {
             id="date"
             className="w-full p-3  border border-gray-100"
             type="date"
+            {...register("date", {
+              required: "La fecha de alta es obligatoria",
+            })}
           />
+
+          {errors.date && <Error>{errors.date?.message?.toString()}</Error>}
         </div>
 
         <div className="mb-5">
@@ -92,7 +108,12 @@ export default function PatientForm() {
             id="symptoms"
             className="w-full p-3  border border-gray-100"
             placeholder="Síntomas del paciente"
+            {...register("symptoms", {
+              required: "Los síntomas son obligatorios",
+            })}
           ></textarea>
+
+          {errors.symptoms && <Error>{errors.symptoms?.message?.toString()}</Error>}
         </div>
 
         <input
